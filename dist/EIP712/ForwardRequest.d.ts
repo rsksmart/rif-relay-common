ca5f5245cfb2ade65f316c43d310c17b45a33986
mortelli <ortellimarcelo@gmail.com>
2022-01-04 16:58:11 -0300

    make collectorContract field optional

--- dist/EIP712/ForwardRequest.d.ts
+++ dist/EIP712/ForwardRequest.d.ts
@@ -4,7 +4,7 @@ export interface ForwardRequest {
     from: string;
     to: string;
     tokenContract: string;
-    collectorContract: string;
+    collectorContract?: string;
     value: string;
     gas: string;
     nonce: string;
