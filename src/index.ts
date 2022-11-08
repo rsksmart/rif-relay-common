438c280bbb301f9699d5c6603d91c5c15313ee33
Christos Otarola <christos@Christoss-MacBook-Pro.local>
2022-11-08 01:28:54 -0500

    feat: export ContractInteractor type

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import type EnvelopingTransactionDetails from '../types/EnvelopingTransactionDetails';
 import type VersionsManager from './VersionsManager';
+import type ContractInteractor from './ContractInteractor';
 
 export * from '../types';
 // export * from './VersionRegistry';
 
-export { EnvelopingTransactionDetails, VersionsManager };
+export { EnvelopingTransactionDetails, VersionsManager, ContractInteractor };
