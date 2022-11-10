443e672d3ccfabc3cebe5e1601390d5f2863f817
Christos Otarola <christos@Christoss-MacBook-Pro.local>
2022-11-10 08:58:16 -0500

    fix: contract interactor is not a type

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import type EnvelopingTransactionDetails from '../types/EnvelopingTransactionDetails';
 import type VersionsManager from './VersionsManager';
-import type ContractInteractor from './ContractInteractor';
+import ContractInteractor from './ContractInteractor';
 
 export * from '../types';
 // export * from './VersionRegistry';
